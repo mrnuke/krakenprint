7b2e3f943bde17da94b7b67a733afafb0388ea24
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2018-05-06 14:19:12 -0500

    test/pixma_parse: Dump unknonwn packets in canonical hex format
    It produces more manageable output, and makes it easier to see
    patterns in the unknonwn data.

--- test/pixma_parse.c
+++ test/pixma_parse.c
@@ -988,10 +988,7 @@ static int process(FILE* in, FILE* out,int verbose,unsigned int maxw,unsigned in
 				break;
 			default: /* Last but not least completely unknown commands */
 				printf("ESC (%c UNKNOWN (len=%i)\n",cmd,cnt);
-				for(i=0;i<cnt;i++)
-					printf(" 0x%x",buf[i]);
-				printf("\n");
-
+				hexdump(buf, cnt);
 		}
 	}
 
